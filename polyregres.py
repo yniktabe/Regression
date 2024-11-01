5553a4d1f6e09f85af30a4301ad9ef2d269c0211
yniktabe <yniktabe1997@gmail.com>
2024-11-01 18:49:13 +0330

    Add files via upload

---
+++ polyregres.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy
+from sklearn.metrics import r2_score
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+mymodel = numpy.poly1d(numpy.polyfit(x,y,3))
+X = numpy.linspace(1,22,100)
+r = r2_score(y ,mymodel(x))
+print(r)
+plt.plot(X ,mymodel(X))
+plt.scatter(x,y)
+plt.show()
